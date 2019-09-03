6f07d9d671fc9111ed5d6c443f4bbb986cc23e36
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-09-03 10:18:48 -0700

    dogs GET route, #tested

--- test/dogs.test.js
+++ test/dogs.test.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Dog = require('../lib/models/Dog');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +15,41 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  beforeEach(async() => {
+    JSON.parse(JSON.stringify(await Dog.create({
+      name: 'Balloon',
+      age: 6,
+      weight: 34
+    })));
+
+    JSON.parse(JSON.stringify(await Dog.create({
+      name: 'Twizzler',
+      age: 3,
+      weight: 17
+    })));
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('gets all the dogs with GET route', () => {
+    return request(app)
+      .get('/api/v1/dogs')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.any(String),
+          name: 'Balloon',
+          age: 6,
+          weight: 34,
+          __v: 0
+        }, {
+          _id: expect.any(String),
+          name: 'Twizzler',
+          age: 3,
+          weight: 17, 
+          __v: 0
+        }]);
+      });
   });
 });
